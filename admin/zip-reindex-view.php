8d25abfea6b429ea0dfd148c5d397d3f3619e68e
Remco Tolsma <info@remcotolsma.nl>
2013-12-27 10:51:25 +0100

    Use the ignore array from the deploy object.

--- admin/zip-reindex-view.php
+++ admin/zip-reindex-view.php
@@ -2,17 +2,6 @@
 
 $zip = $deploy->zip;
 
-$ignore = array(
-	'.buildpath',
-	'.gitignore',
-	'.project',
-	'.settings/*',
-	'bin/*',
-	'tests/*',
-	'phpunit.xml',
-	'.travis.yml',
-);
-
 function should_ignore( $file, $patterns ) {
 	$ignore = false;
 
@@ -39,10 +28,6 @@ function rename_root( $file, $old_root ) {
 <table class="wp-list-table widefat fixed" cellspacing="0">
 	<thead>
 		<tr>
-			<th style="" class="manage-column column-cb check-column" id="cb" scope="col">
-				<label for="cb-select-all-1" class="screen-reader-text">Alle selecteren</label>
-				<input type="checkbox" id="cb-select-all-1" />
-			</th>
 			<th style="" class="manage-column column-title" id="title" scope="col">
 				<?php _e( 'Path', 'pronamic_wp_extensions' ); ?>
 			</th>
@@ -65,16 +50,11 @@ function rename_root( $file, $old_root ) {
 			<tr>
 				<?php 
 				
-				$base = rename_root( $item_name, $deploy->zip_dir );
+				$base = rename_root( $item_name, $deploy->zip_dir . '/' );
 				
-				$rename = $deploy->zip_dir_new . $base;
+				$rename = $deploy->zip_dir_new . '/' . $base;
 				
 				?>
-				<th class="check-column" scope="row">
-					<label for="cb-select-<?php echo $i; ?>" class="screen-reader-text">Test selecteren</label>
-
-					<input type="checkbox" value="<?php echo esc_attr( $i ); ?>" name="entries[<?php echo $i; ?>][checkbox]" id="cb-select-<?php echo $i; ?>" />
-				</th>
 				<td>
 					<?php echo $item_name; ?>
 				</td>
@@ -86,7 +66,7 @@ function rename_root( $file, $old_root ) {
 				<td>
 					<label for="cb-ignore-<?php echo $i; ?>" class="screen-reader-text">Test ignore</label>
 
-					<input type="checkbox" value="102" name="entries[<?php echo $i; ?>][ignore]" id="cb-ignore-<?php echo $i; ?>" <?php checked( should_ignore( $base, $ignore ) ); ?> />
+					<input type="checkbox" value="102" name="entries[<?php echo $i; ?>][ignore]" id="cb-ignore-<?php echo $i; ?>" <?php checked( should_ignore( $base, $deploy->ignore ) ); ?> />
 				</td>
 			</tr>
 		
