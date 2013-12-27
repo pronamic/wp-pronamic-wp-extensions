15a98adf46ec877c6cd0b8db7e5baab9feba8dca
Remco Tolsma <info@remcotolsma.nl>
2013-12-27 10:50:19 +0100

    Remove checkbox column.

--- admin/zip-view.php
+++ admin/zip-view.php
@@ -1,10 +1,6 @@
 <table class="wp-list-table widefat fixed" cellspacing="0">
 	<thead>
 		<tr>
-			<th style="" class="manage-column column-cb check-column" id="cb" scope="col">
-				<label for="cb-select-all-1" class="screen-reader-text">Alle selecteren</label>
-				<input type="checkbox" id="cb-select-all-1" />
-			</th>
 			<th style="" class="manage-column column-title" id="title" scope="col">
 				<?php _e( 'Name', 'pronamic_wp_extensions' ); ?>
 			</th>
@@ -18,11 +14,6 @@
 		<?php  while ( $item_name = $zip->getNameIndex( $i ) ) : ?>
 
 			<tr>
-				<th class="check-column" scope="row">
-					<label for="cb-select-<?php echo $i; ?>" class="screen-reader-text">Test selecteren</label>
-
-					<input type="checkbox" value="<?php echo esc_attr( $i ); ?>" name="entries[<?php echo $i; ?>][checkbox]" id="cb-select-<?php echo $i; ?>" />
-				</th>
 				<td>
 					<?php echo $item_name; ?>
 				</td>
