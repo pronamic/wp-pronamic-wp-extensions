ae9f88dacde5b1df8bad7160a8748c4d85cf1293
Remco Tolsma <info@remcotolsma.nl>
2014-01-03 16:24:59 +0100

    Added total downloads field.

--- admin/meta-box-version.php
+++ admin/meta-box-version.php
@@ -15,5 +15,13 @@ wp_nonce_field( 'pronamic_wp_extension_save_meta_version', 'pronamic_wp_extensio
 				<input id="pronamic_extension_stable_version" name="_pronamic_extension_stable_version" value="<?php echo esc_attr( get_post_meta( $post->ID, '_pronamic_extension_stable_version', true ) ); ?>" type="text" size="25" class="regular-text" />
 			</td>
 		</tr>
+		<tr>
+			<th scope="row">
+				<label for="pronamic_extension_total_downloads"><?php _e( 'Downloads', 'pronamic_wp_extensions' ); ?></label>
+			</th>
+			<td>
+				<input id="pronamic_extension_total_downloads" name="_pronamic_extension_total_downloads" value="<?php echo esc_attr( get_post_meta( $post->ID, '_pronamic_extension_total_downloads', true ) ); ?>" type="text" size="25" class="regular-text" />
+			</td>
+		</tr>
 	</tbody>
 </table>
--- classes/Pronamic/WP/ExtensionsPlugin/Admin.php
+++ classes/Pronamic/WP/ExtensionsPlugin/Admin.php
@@ -423,7 +423,8 @@ class Pronamic_WP_ExtensionsPlugin_Admin {
 			return;
 	
 		$this->save_extension_meta( $post_id, array(
-			'_pronamic_extension_stable_version' => FILTER_SANITIZE_STRING,
+			'_pronamic_extension_stable_version'  => FILTER_SANITIZE_STRING,
+			'_pronamic_extension_total_downloads' => FILTER_SANITIZE_STRING,
 		) );
 	}
 
